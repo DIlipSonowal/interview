42a40ba8844b8faee19eb978a0a79b55c8f9ef89
dilip sonowal <kirankr8420@gmail.com>
2020-05-11 08:20:21 +0530

    kannada

--- src/app/kannada/kannada.component.ts
+++ src/app/kannada/kannada.component.ts
@@ -113,7 +113,7 @@ export class KannadaComponent implements OnInit {
       `
     },
     {
-      id:2,
+      id:3,
       link:'past',
       q:'Past Tense',
       a:`<h3>Past Tense</h3>
@@ -131,7 +131,8 @@ export class KannadaComponent implements OnInit {
         <td>I had food.</td>
         <td>Nanu oota marday.<br/>
         For asking, oota mardiya? <br/>
-        Marday/Maridila
+        Marday/Maridila<br/>
+        Oota aita? -> aitu/agila - thin-thini
         </td>
       </tr>
       <tr>
@@ -140,6 +141,298 @@ export class KannadaComponent implements OnInit {
             Mane inda bonday (already reached)
             Mane inda bondidini. - Office inda bondidini etc
         </td>
+      </tr>
+      </table>
+      `
+    },
+    {
+      id:4,
+      link:'future',
+      q:'Future Tense',
+      a:`<h3>Future Tense</h3>
+      <table width="100%" border="1" cellpadding="2">
+      <tr>
+        <th>English</th>
+        <th>Kannada</th>
+      </tr>
+      <tr>
+        <td colspan="2" align="center">
+          Word + thini (borthini, marthini, thin-thini etc)
+        </td>
+      </tr> 
+      <tr>
+        <td>I will come</td>
+        <td>Nanu borthini
+        <br/>
+        I will do - Nanu marthini<br/>
+        Oota aita? - aitu/agila - thin thini
+        </td>                        
+      </tr>
+      <tr>
+        <td>I will go to office</td>
+        <td>Nanu office gay hogthini</td>
+      </tr>
+      </table>`
+    },
+    {
+      id:5,
+      link:'opposite',
+      q:'Opposite word',
+      a:`<h3>Opposite word</h3>
+      <table width="100%"  cellpadding="2">
+        <tr>
+          <td valign="top">
+            <table width="100%" border="1">
+                <tr>
+                   <td>I (Nanu)</td>
+                   <td>You(Ninu/Nivu)</td>
+                </tr>
+                <tr>
+                   <td>He (Avanu/Avaru)</td>
+                   <td>She(Avalu/Avaru)</td>
+                </tr>
+                <tr>
+                   <td>Come (Ba/Banni)</td>
+                   <td>Go(Hogu/Hogi)</td>
+                </tr>
+                <tr>
+                   <td>They (Avaru)</td>
+                   <td>We(Navu)</td>
+                </tr>
+                <tr>
+                   <td>Day (Dina)</td>
+                   <td>Night(Ratri)</td>
+                </tr>
+                <tr>
+                   <td>Speed (Bega)</td>
+                   <td>Slow(Mela)</td>
+                </tr>
+                <tr>
+                   <td>Hot (Bisi)</td>
+                   <td>Cold(Thanna)</td>
+                </tr>
+                <tr>
+                   <td>Sister (tnaggi)</td>
+                   <td>Brother(Aanna)</td>
+                </tr>
+                <tr>
+                   <td>Mother(Aamma)</td>
+                   <td>Father(Aappa)</td>
+                </tr>
+                <tr>
+                  <td>Right (Balagade)</td>
+                  <td>Left(Edagade)</td>
+                </tr>
+                <tr>
+                  <td>My/Mine (Nanna/Nannadu)</td>
+                  <td>Your(Ninna/Nimma)</td>
+                </tr>
+                <tr>
+                  <td>Our (Namma)</td>
+                  <td>Their(Aavaru)</td>
+                </tr>
+                <tr>
+                  <td>Here (Elli)</td>
+                  <td>There(Aali)</td>
+                </tr>
+                <tr>
+                  <td>This/These (Edu/Evu)</td>
+                  <td>That/Those(Aadu/Aastu)</td>
+                </tr>
+                
+            </table>
+          </td>
+          <td valign="top">
+          <table width="100%" border="1">
+              <tr>
+                <td>Today (Ebbatoo)</td>
+                <td>Yesterday (Ninne) Tomorrow (Naale)</td>
+              </tr>
+              <tr>
+                <td>Front (Munday)</td>
+                <td>Back (Hinday)</td>
+              </tr>
+              <tr>
+                <td>Door (Bagilu/Kadda)</td>
+                <td>Window (Kitaki)</td>
+              </tr>
+              <tr>
+                <td>Give (Koro/kori)</td>
+                <td>Take (Togoli)</td>
+              </tr>
+              <tr>
+                <td>Give (Koro/kori)</td>
+                <td>Take (Togoli)</td>
+              </tr>
+              <tr>
+                <td>Sit (Khootko/khootkoli)</td>
+                <td>Stand (Nindko/nindkolli)</td>
+              </tr>
+              <tr>
+                <td>See (Noori)</td>
+                <td>Donot see (Nore beri)</td>
+              </tr>
+              <tr>
+                <td>Open (Teggi)</td>
+                <td>Close(Moocchu)</td>
+              </tr>
+              <tr>
+                <td>Big (Dordoo)</td>
+                <td>Small(Chikku)</td>
+              </tr>
+              <tr>
+                <td>Up (Mele)</td>
+                <td>Down(Kelage)</td>
+              </tr>
+              <tr>
+                <td>Out (Horage/aache)</td>
+                <td>In(Olage)</td>
+              </tr>
+              <tr>
+                <td>Now (Ebaga/Ega)</td>
+                <td>Then/ After(Aamele)</td>
+              </tr>
+              <tr>
+                <td>Done (Aagiday)</td>
+                <td>Did not(Aagila)</td>
+              </tr>
+          </table>
+          </td>
+        </tr>
+      </table>
+      `
+    },
+    {
+      id:6,
+      q:'Common words',
+      link:'comnwrds',
+      a:`
+      <table width="100%" border="1" cellpadding="2">
+      <tr>
+        <th>English</th>
+        <th>Kannada</th>
+        <th>English</th>
+        <th>Kannada</th>
+      </tr>
+      <tr>
+        <td>Coconut</td>
+        <td>Tenginakaie</td>
+        <td>Banna</td>
+        <td>Balehannu</td>
+      </tr>
+      <tr>
+        <td>Milk (put milk)</td>
+        <td>Halu (halu Hako/Haki)</td>
+        <td>Aagarbotti</td>
+        <td>Uddinakaddi</td>
+      </tr>
+      <tr>
+        <td>Rice</td>
+        <td>Hakki</td>
+        <td>Water</td>
+        <td>Niru</td>
+      </tr>
+      <tr>
+        <td>Turmarik</td>
+        <td>Haricina</td>
+        <td>Soil</td>
+        <td>Mannu</td>
+      </tr>
+      <tr>
+        <td>House</td>
+        <td>Mane</td>
+        <td>Finger</td>
+        <td>Berelu</td>
+      </tr>
+      <tr>
+        <td>Oil</td>
+        <td>Enne</td>
+        <td>Floor</td>
+        <td>Nela</td>
+      </tr>
+      <tr>
+        <td>Plate</td>
+        <td>Thatte</td>
+        <td>Wash</td>
+        <td>Toli (tolitaidare/tolitare/toledru)</td>
+      </tr>
+      <tr>
+        <td>Plate</td>
+        <td>Thatte</td>
+        <td>And</td>
+        <td>Matte</td>
+      </tr>
+      <tr>
+        <td>Hand</td>
+        <td>Kaye</td>
+        <td>Foot</td>
+        <td>Kalu</td>
+      </tr>
+      <tr>
+        <td>Eye</td>
+        <td>Kannu</td>
+        <td>Stomach</td>
+        <td>Otte</td>
+      </tr>
+      <tr>
+        <td>Ear</td>
+        <td>Kibi</td>
+        <td>Nose</td>
+        <td>Moogu</td>
+      </tr>
+      <tr>
+        <td>Mouth</td>
+        <td>Baye</td>
+        <td>Tooth</td>
+        <td>Hallu</td>
+      </tr>
+      <tr>
+        <td>Tongue</td>
+        <td>Nalige</td>
+        <td>Hair</td>
+        <td>Hoodalu</td>
+      </tr>
+      <tr>
+        <td>Why</td>
+        <td>Yakke</td>
+        <td>When</td>
+        <td>Yabaga</td>
+      </tr>
+      <tr>
+        <td>Enough</td>
+        <td>Saaku</td>
+        <td>Want</td>
+        <td>Beku</td>
+      </tr>
+      <tr>
+        <td>Common</td>
+        <td>Normal</td>
+        <td>Memorize (Keep in mind)</td>
+        <td>Nenapu (Nenapinalli itko)</td>
+      </tr>
+      <tr>
+        <td>Or</td>
+        <td>Authowa</td>
+        <td>What</td>
+        <td>Yenu</td>
+      </tr>
+      <tr>
+        <td>Means</td>
+        <td>Artho</td>
+        <td>Understand</td>
+        <td>Artho agutiday</td>
+      </tr>
+      <tr>
+        <td>Is it?</td>
+        <td>Howda?</td>
+        <td>Yes/It is</td>
+        <td>Howdu</td>
+      </tr>
+      <tr>
+        <td>---</td>
+        <td>---</td>
+        <td>---</td>
+        <td>---</td>
       </tr>
       </table>
       `
